87d753126363ff618efcaf633defbf7a2ff7cf90
fpezcara <fpezcara@gmail.com>
2022-06-30 15:00:22 +0100

    update testing

--- test/api.test.js
+++ test/api.test.js
@@ -5,19 +5,6 @@ const app = require("../server.js");
 describe("API server", () => {
   let api;
 
-  const testNewPost = {
-    id: 1,
-    content: "user input",
-    giphy: [],
-    created: "02/06/2022",
-    reactions: {
-      love: 0,
-      like: 0,
-      dislike: 0,
-    },
-    comments: [],
-  };
-
   beforeAll(() => {
     api = app.listen(7005, () =>
       console.log(`Test server running on port 7005`)
@@ -54,7 +41,7 @@ describe("API server", () => {
   //       done
   //     );
   // });
-
+  //this test will fail if anything changes in the post with id 1 at posts.json
   it("retrieves a post by id", (done) => {
     request(api)
       .get("/posts/1")
@@ -65,7 +52,7 @@ describe("API server", () => {
           content: "user input",
           giphy: "",
           created: "02/06/2022",
-          reactions: [{ love: 0, like: 5, dislike: 2 }],
+          reactions: { love: 0, like: 5, dislike: 2 },
           comments: [],
         },
         done
@@ -77,6 +64,49 @@ describe("API server", () => {
   // it("responds with a status of 201 at /posts/1", (done) => {
   //   request(api).post("/posts/1").expect(201).expect(testNewPost, done);
   // });
+
+  it("responds to get /posts/1/comments with status 200", (done) => {
+    request(api).get("/posts/1/comments").expect(200, done);
+  });
+
+  it("responds to get /posts/17af4l4zs5vim/comments/2hj8fx39csl5104ruk with status 200", (done) => {
+    request(api)
+      .get("/posts/17af4l4zs5vim/comments/2hj8fx39csl5104ruk")
+      .expect(200, done);
+  });
+  // it("responds to post /posts/post with status 201", (done) => {
+  //   request(api)
+  //     .post("/posts/post")
+  //     .send({ content: "hey there", giphy: "" })
+  //     .set("Accept", /application\/json/)
+  //     .expect(201)
+  //     .expect(
+  //       {
+  //         id: "jskjs8js", //how do i test this? do i import the package then mock it?
+  //         content: "Hey there",
+  //         giphy: "",
+  //       },
+  //       done
+  //     );
+  // });
+
+  // it("responds to put /posts/1/reactions/like", (done) => {
+  //   request(api)
+  //     .get("/posts/1")
+  //     .expect(200)
+  //     .expect(
+  //       {
+  //         id: 1,
+  //         content: "user input",
+  //         giphy: "",
+  //         created: "02/06/2022",
+  //         reactions: [{ love: 0, like: 5, dislike: 2 }],
+  //         comments: [],
+  //       },
+  //       done
+  //     );
+  // });
+
   it("responds to a unknown post id with a 404", (done) => {
     request(api).get("/posts/42").expect(404).expect({}, done);
   });
---
+++ test/models.test.js
@@ -0,0 +1,57 @@
+const postsData = require("../data/posts.json");
+const Post = require("../models/post.js");
+const Comment = require("../models/comment.js");
+const uniqid = require("uniqid");
+jest.mock("uniqid");
+describe("Models", () => {
+  describe("Post", () => {
+    const id = uniqid();
+    const newDate = new Date();
+    const testNewPost = {
+      id,
+      content: "user input",
+      giphy: "",
+      created: newDate,
+      reactions: {
+        love: 0,
+        like: 0,
+        dislike: 0,
+      },
+      comments: [],
+    };
+    it("should make an instance of a post", () => {
+      const post = new Post(testNewPost);
+
+      expect(post.id).toBe(id);
+      expect(post.content).toBe("user input");
+      expect(post.created).toBe(newDate);
+    });
+
+    it("should return all posts", () => {
+      const posts = Post.all;
+
+      expect(posts).toEqual(postsData);
+    });
+
+    it("should return a post", () => {
+      const post = Post.findById(1);
+
+      expect(post).toEqual(postsData[0]);
+    });
+
+    it("should throw an error if no post found", () => {
+      function testError() {
+        Post.findById(50);
+      }
+
+      expect(testError).toThrowError("Post not found");
+    });
+
+    // it("should create a post", () => {
+    //   //   const newPostId = uniqid();
+    //   const newPost = Post.create(testNewPost);
+
+    //   expect(newPost).toEqual(testNewPost);
+    // });
+  });
+});
